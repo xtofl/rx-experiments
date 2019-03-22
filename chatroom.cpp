c207e5dc18f39b15ea72637798d6d3d0f1d7de46
xtofl <kristoffel.pirard@gmail.com>
2019-03-22 18:45:37 +0100

    ... at least it compiles

--- chatroom.cpp
+++ chatroom.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
+#include <sstream>
 #include "rxcpp/rx.hpp"
 #include "boost/asio.hpp"
+#include "boost/asio/async_result.hpp"
+
 
 
 namespace Rx {
@@ -19,9 +22,26 @@ int main() {
  
     context.post([&]{
         tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), 2000));
-        auto connection = tcp::socket(context);
-        acceptor.async_accept(connection,
-                              [&](auto &error) {});
+        auto socket = std::make_shared<tcp::socket>(context);
+        acceptor.async_accept(
+            *socket,
+            [socket=std::move(socket), &context] (auto &error)
+        {
+            if(error) return;
+            auto r = context.post([socket=std::move(socket)]
+            {
+                auto buffer = std::make_shared<boost::asio::streambuf>();
+
+                auto r = boost::asio::async_read(*socket, *buffer, [buffer=std::move(buffer)]
+                (const auto &error, const auto number_of_bytes)
+                {
+                    printf("%lu bytes READ\n", number_of_bytes);
+                });
+                puts("CONNECTED!\n");
+                return r;
+            });
+            return r;
+        });
         puts("Accepted\n");
     });
     context.run();
