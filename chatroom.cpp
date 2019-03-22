b6fe917323d5b5cb331f6d412ae23fe9b9fe75bb
xtofl <kristoffel.pirard@gmail.com>
2019-03-22 07:41:43 +0100

    wIP: closer to an accepting server

--- chatroom.cpp
+++ chatroom.cpp
@@ -1,6 +1,29 @@
 #include <cstdio>
+#include "rxcpp/rx.hpp"
+#include "boost/asio.hpp"
+
+
+namespace Rx {
+using namespace rxcpp;
+using namespace rxcpp::sources;
+using namespace rxcpp::operators;
+using namespace rxcpp::util;
+}
+using namespace Rx;
+
 
 int main() {
-    printf("Starting Chatroom\n");
-    printf("Closed Chatroom\n");
+    puts("Starting Chatroom\n");
+    using namespace boost::asio::ip;
+    boost::asio::io_service context;
+ 
+    context.post([&]{
+        tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), 2000));
+        auto connection = tcp::socket(context);
+        acceptor.async_accept(connection,
+                              [&](auto &error) {});
+        puts("Accepted\n");
+    });
+    context.run();
+    puts("Closed Chatroom\n");
 }
