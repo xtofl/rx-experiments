dcb0daee1f76bd83d7724f3421c02ae85e60ae3e
xtofl <kristoffel.pirard@gmail.com>
2019-03-23 14:05:30 +0100

    Solve async pitfall: object lifetime

--- chatroom.cpp
+++ chatroom.cpp
@@ -19,28 +19,28 @@ int main() {
     puts("Starting Chatroom\n");
     using namespace boost::asio::ip;
     boost::asio::io_service context;
- 
-    context.post([&]{
-        tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), 2000));
+
+    tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), 2000));
+    context.post([&context, &acceptor]{
         auto socket = std::make_shared<tcp::socket>(context);
         acceptor.async_accept(
             *socket,
-            [socket=std::move(socket), &context] (auto &error)
+            [socket, &context] (auto &error)
         {
             if(error) return;
-            auto r = context.post([socket=std::move(socket)]
+
+            context.post([socket=std::move(socket)]
             {
                 auto buffer = std::make_shared<boost::asio::streambuf>();
 
-                auto r = boost::asio::async_read(*socket, *buffer, [buffer=std::move(buffer)]
+                boost::asio::async_read(*socket, *buffer, [buffer]
                 (const auto &error, const auto number_of_bytes)
                 {
                     printf("%lu bytes READ\n", number_of_bytes);
+                    puts(boost::asio::buffer_cast<const char*>(buffer->data()));
                 });
                 puts("CONNECTED!\n");
-                return r;
             });
-            return r;
         });
         puts("Accepted\n");
     });
